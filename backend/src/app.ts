130e518c61cd9e986463f4da84937da42a3eeed8
Levi Lunique <levilunique@Levis-MacBook-Air.local>
2024-07-21 18:13:31 -0300

    [FEATURE-SL-8] Create productRoutes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,10 @@
-import express, { Express } from 'express'
+import express, { Express } from 'express';
+import productRoutes from './routes/productRoutes';
 
 const app: Express = express();
 
 app.use(express.json());
+app.use('/products', productRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
