beffbf509221e2134e9d00d934d0788465f8ed87
Levi Lunique <levilunique@Levis-MacBook-Air.local>
2024-07-21 18:00:24 -0300

    [FEATURE-SL-8] Create productController

---
+++ backend/src/controllers/productController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const createProduct = async (req: Request, res: Response) => {
+    const { name, value } = req.body;
+
+    try {
+        const product = await prisma.product.create({
+            data: {
+                name,
+                value,
+            }
+        });
+        res.status(201).json(product);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
